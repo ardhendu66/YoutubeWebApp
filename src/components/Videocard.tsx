d0e271ddcc1698587cb66b93073831a1426bb58a
ardhenduroy67 <95483892+ardhenduroy67@users.noreply.github.com>
2024-02-08 15:41:44 +0530

    f

---
+++ src/components/Videocard.tsx
@@ -0,0 +1,25 @@
+
+
+export default () => {
+    return (
+        <div>
+            <img src="/YT.jpg" />
+            <div className="grid grid-cols-12 pt-4">
+                <div className="col-span-1 w-20 h-20 flex justify-center items-center">
+                    <img src="/YTlogo.jpg" className="w-full h-full rounded-full" />
+                </div>
+                <div className="col-span-11 pl-10">
+                    <div>
+                        Jawan: Chaleya (Hindi) | Shah Rukh Khan
+                    </div>
+                    <div className="col-span-11 text-gray-600 text-base">
+                        Harkirat Singh
+                    </div>
+                    <div className="col-span-11 text-gray-600 text-base">
+                        46M | 13 days ago
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
