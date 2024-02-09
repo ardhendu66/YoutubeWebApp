8c6452522a2426a139fa06cb1ae24707d21d37df
ardhenduroy67 <95483892+ardhenduroy67@users.noreply.github.com>
2024-02-09 20:13:26 +0530

    commit

--- src/components/Videogrid.tsx
+++ src/components/Videogrid.tsx
@@ -5,7 +5,7 @@ export default () => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-5">
             {
-                VIDEOS.map(video => <div key={video!.id}>
+                VIDEOS.map(video => <div key={Number(video!.id)}>
                         <Videocard video={video} />
                     </div>
                 )
